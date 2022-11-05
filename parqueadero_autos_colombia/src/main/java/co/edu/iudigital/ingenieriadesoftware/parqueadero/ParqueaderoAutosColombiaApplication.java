ac0de056e04aa164f65200b00b6a177bdeca8d87
mateo-grajales-iud <mateo.grajales@est.iudigital.edu.co>
2022-11-05 17:46:08 -0500

    Creacion proyecto Spring (Back)
    Se crea el proyecto que va a ser el back de la app

---
+++ parqueadero_autos_colombia/src/main/java/co/edu/iudigital/ingenieriadesoftware/parqueadero/ParqueaderoAutosColombiaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.iudigital.ingenieriadesoftware.parqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoAutosColombiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoAutosColombiaApplication.class, args);
+	}
+
+}
---
+++ parqueadero_autos_colombia/src/test/java/co/edu/iudigital/ingenieriadesoftware/parqueadero/ParqueaderoAutosColombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.iudigital.ingenieriadesoftware.parqueadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoAutosColombiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
