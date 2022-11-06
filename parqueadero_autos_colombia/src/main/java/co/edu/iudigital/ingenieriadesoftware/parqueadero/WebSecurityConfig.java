720ba40041ce8cd451da743906f2a418016cd381
mateo-grajales-iud <mateo.grajales@est.iudigital.edu.co>
2022-11-06 11:49:12 -0500

    correccion

--- parqueadero_autos_colombia/src/main/java/co/edu/iudigital/ingenieriadesoftware/parqueadero/WebSecurityConfig.java
+++ parqueadero_autos_colombia/src/main/java/co/edu/iudigital/ingenieriadesoftware/parqueadero/WebSecurityConfig.java
@@ -1,16 +1,33 @@
 package co.edu.iudigital.ingenieriadesoftware.parqueadero;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-@Configuration
-public class WebSecurityConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+    
 }
